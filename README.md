23715c9c05aee07c89786af3fa015fe3b12fb892
cyhaz <cyhaz@naver.com>
2020-10-18 12:31:35 +0900

    finish project

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Movie App 2020
 
 React JS Fundamentals Course (2019 Update!)
+
+hompage : "https://cyhaz.github.io/movie_app_2020/"
---
+++ src/routes/About.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function About() {
+  return <h1>About this page : 이 페이지는 영화목록 페이지</h1>;
+}
+
+export default About;
